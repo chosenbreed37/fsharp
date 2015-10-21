d499442d770dcc98036008b9e25304ccbcefe197
Eric Lubisse <chosenbreed@gmail.com>
2015-10-21 15:32:54 +0100

    A few updates...

---
+++ misc/active-directory.fsx
@@ -0,0 +1,43 @@
+﻿#r "System.DirectoryServices"
+#r "System.DirectoryServices.AccountManagement.dll"
+
+open System.DirectoryServices
+open System.DirectoryServices.AccountManagement
+
+let ad = new PrincipalContext(ContextType.Domain, "edftrading.com")
+let createSearcher o = new PrincipalSearcher(o)
+
+let asGroup id =            
+    let g = new GroupPrincipal(ad)
+    g.SamAccountName <- id
+    g
+
+let asUser id =            
+    let u = new UserPrincipal(ad)
+    u.SamAccountName <- id
+    u
+
+let queryAd withPrincipal id =
+    use searcher = id |> withPrincipal |> createSearcher
+    let result = searcher.FindOne()
+    if result <> null then
+        Some result
+    else 
+        None
+
+type OrElseBuilder() =
+    member __.ReturnFrom(x) = x
+    
+    member __.Combine(a, b) =
+        match a with
+        | Some _ -> a
+        | None -> b
+    
+    member __.Delay(f) = f()
+
+let orElse = new OrElseBuilder()
+
+let adQuery id = orElse {
+    return! queryAd asGroup id
+    return! queryAd asUser id
+}
